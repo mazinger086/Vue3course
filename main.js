b0582a4b0c7ace8cc65a8c85184c95e767b7a508
Daniel Lasauskas <Mazinger_086@yahoo.com>
2022-09-22 09:35:15 -0300

    Creating Vue App - clase 01

---
+++ main.js
@@ -0,0 +1,8 @@
+const app = Vue.createApp({
+    data: function(){
+        return {
+            product:'Shoes',
+            description: 'It\'s something you use in your feet'
+        }
+    }
+})
